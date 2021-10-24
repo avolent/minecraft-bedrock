09972ea8359cc32e0d9ea1abc07cd40f69dfe0c3
avolent <will@slamped.com>
2021-10-24 17:11:29 +1100

    added import os

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# minecraft-bedrock
-A minecraft bedrock dockerfile
+# Minecraft-bedrock Server in Docker
+The following is a Minecraft-bedrock server in which i will be using in AWS ECS.
--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
+import os
+
 print("Downloading Minecraft Bedrock Server")
 # Download - https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip
-wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip
+os.system("wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip")
 
 print("Unzipping and clean up")
-unzip bedrock-server-1.17.40.06.zip && rm bedrock-server-1.17.40.06.zip
+os.system("unzip bedrock-server-1.17.40.06.zip && rm bedrock-server-1.17.40.06.zip")
 
 print("Starting Server")
-./bedrock_server
+os.system("./bedrock_server")
