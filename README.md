25f15002c16a0513209380a3368a7bad248f44f4
avolent <will@slamped.com>
2021-10-24 17:49:32 +1100

    updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Minecraft-bedrock Server in Docker
 The following is a Minecraft-bedrock server in which i will be using in AWS ECS.
+
+Start server with the following command
+`docker run -it -p 19132:19132/udp minecraft-bedrock`
--- build.py
+++ build.py
@@ -1,5 +1,13 @@
 #! /usr/bin/python
 import os
+#Global Variables
+SERVER_NAME = "test"
+GAMEMODE = ""
+DIFFICULTY = ""
+LEVEL_NAME = ""
+ALLOW_CHEATS = ""
+MAX_PLAYERS = ""
+MAX_THREADS = "0"
 
 print("Downloading Minecraft Bedrock Server")
 # Download - https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40.06.zip
@@ -8,5 +16,8 @@ os.system("wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.17.40
 print("Unzipping and clean up")
 os.system("unzip bedrock-server-1.17.40.06.zip && rm bedrock-server-1.17.40.06.zip")
 
+print("Server Configuration")
+
 print("Starting Server")
+print(SERVER_NAME)
 os.system("./bedrock_server")
